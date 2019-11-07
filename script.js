fcac0dec2e0cfb15973580bbe19bd636b0aea238
gsak3l <gsakel5@gmail.com>
2019-11-07 09:09:31 +0200

    checkwin function added

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function startGame() {
         cells[i].addEventListener('click', turnClick, false);
     }
 }
+
 function turnClick(square) {
     turn(square.target.id, humanPlayer);
 }
@@ -32,4 +33,24 @@ function turnClick(square) {
 function turn(squareId, player) {
     originalBoard[squareId] = player;
     document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(originalBoard, player);
+    if (gameWon) gameOver(gamwWon);
+}
+
+function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) =>
+        (e === player) ? a.concat(i) : a, []);
+    /*the reduce method goes threw every element of the
+    board array and gives back one single value in this case.
+    In this method, the a(ccumulator) is the value that we will get back
+    and we are going to initialize the a to an empty array.
+    the e is the element in the board array, that we are going 
+    threw, and the i is the index. So if the element equals the player
+    then we are going to contact i. That just means that we are gonna
+    take the accimilator array, and we are going to add the index (i) to
+    that array, and then if e doesn't equal player, we are just gonna
+    return the accumulator just as it was so we are not going to add
+    anything to the accumulator.
+    */
+    //this is just a way to find every index that the player has played in
 }
