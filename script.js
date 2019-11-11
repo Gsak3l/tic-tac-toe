905e9cd28f6d8529a0eb4da6a7576ad96dc2bfb4
gsak3l <gsakel5@gmail.com>
2019-11-11 18:43:25 +0200

    lots of functions added

--- script.js
+++ script.js
@@ -87,3 +87,29 @@ function gameOver(gameWon) {
         cells[i].removeEventListener('click', turnClick, false);
     }
 }
+
+function emptySquares() {
+    return originalBoard.filter(s => typeof s == 'number');
+
+}
+
+function bestSpot() {
+    return emptySquares()[0];
+}
+
+function checkTie() {
+    if(emptySquares().length == 0) {
+        for(var i = 0; i < cells.length; i++) {
+            cells[i].style.backgroundColor = "green";
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        declareWinner("Tie Game!");
+        return true;
+    }
+    return false;
+}
+
+function declareWinner(who) {
+    document.querySelector(".endgame").style.display = "block";
+    document.querySelector(".endgame .text").innerText = who;
+}
